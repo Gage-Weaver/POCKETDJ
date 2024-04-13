3e6c5561b1390d54c3561368888fbeef095a222e
Carlos Mbendera <carlosmbendera@gmail.com>
2024-04-13 13:43:54 -0500

    Parse AI Model API Response Pretty Neatly

--- AIStuff.py
+++ AIStuff.py
@@ -1,5 +1,7 @@
 import requests
 
+
+Api_Token = "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"
 API_URL = "https://api-inference.huggingface.co/models/trpakov/vit-face-expression"
 headers = {"Authorization": f"Bearer {Api_Token}"}
 
@@ -7,3 +9,5 @@ def query(filename):
     data = filename
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()
+
+
