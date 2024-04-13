258e428d753d2bc39053940f5848e2763837c50c
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-12 21:04:26 -0500

    Update UserInterface.py
    Update Comments

--- UserInterface.py
+++ UserInterface.py
@@ -1,5 +1,6 @@
 ###User Interface
-import streamlit as st
+import streamlit as st ###Importing Streamlit 
 st.camera_input("Users Webcam", key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False, label_visibility="visible")
 st.file_uploader("Upload your Picture", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+
 
