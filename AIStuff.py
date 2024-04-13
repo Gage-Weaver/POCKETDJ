b31f6eb4f95d6491ef830bcf51ca6ff4415e8653
Carlos Mbendera <carlosmbendera@gmail.com>
2024-04-12 21:05:42 -0500

    Added AI Function Detection With Hugging Face

---
+++ AIStuff.py
@@ -0,0 +1,14 @@
+import requests
+
+API_URL = "https://api-inference.huggingface.co/models/trpakov/vit-face-expression"
+headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+def query(filename):
+    with open(filename, "rb") as f:
+        data = f.read()
+    response = requests.post(API_URL, headers=headers, data=data)
+    return response.json()
+
+output = query("fire.jpg")
+
+print(output)
