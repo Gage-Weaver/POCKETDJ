8f7dc1e2c81206c0570939491e5605060dad3cbc
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-12 21:21:14 -0500

    File Updates
    Update Query Function and User Interface

--- AIStuff.py
+++ AIStuff.py
@@ -1,14 +1,9 @@
 import requests
 
 API_URL = "https://api-inference.huggingface.co/models/trpakov/vit-face-expression"
-headers = {"Authorization": f"Bearer {API_TOKEN}"}
+headers = {"Authorization": f"Bearer {Api_Token}"}
 
 def query(filename):
-    with open(filename, "rb") as f:
-        data = f.read()
+    data = filename
     response = requests.post(API_URL, headers=headers, data=data)
     return response.json()
-
-output = query("fire.jpg")
-
-print(output)
--- UserInterface.py
+++ UserInterface.py
@@ -2,7 +2,7 @@
 import streamlit as st ###Importing Streamlit 
 from AIStuff import query
 st.camera_input("Users Webcam", key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False, label_visibility="visible")
-file_upload= st.file_uploader("Upload your Picture", type='jpg', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+file_upload = st.file_uploader("Upload your Picture", type='jpg', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 if file_upload is not None:
     st.write(query(file_upload))
 
