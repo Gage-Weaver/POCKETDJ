1037450f08b08aa21a598934b5e32cfedb394eb0
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-12 20:37:59 -0500

    Update UserInterface.py
    Added Camera and File Input

--- UserInterface.py
+++ UserInterface.py
@@ -1,1 +1,5 @@
 ###User Interface
+import streamlit as st
+st.camera_input("Users Webcam", key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False, label_visibility="visible")
+st.file_uploader("Upload your Picture", type=None, accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
+
