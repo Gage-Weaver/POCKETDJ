81427bae6bd578a4d1376e8ba5aa73193711b0e4
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-13 11:24:05 -0500

    Update UserInterface.py
    Adding Camera Input Emotion Classification

--- UserInterface.py
+++ UserInterface.py
@@ -1,8 +1,10 @@
 ###User Interface
 import streamlit as st ###Importing Streamlit 
-from AIStuff import query
-st.camera_input("Users Webcam", key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False, label_visibility="visible")
+from AIStuff import query ###Import Query
+camera_input= st.camera_input("Users Webcam", key=None, help=None, on_change=None, args=None, kwargs=None,disabled=False, label_visibility="visible")
 file_upload = st.file_uploader("Upload your Picture", type='jpg', accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False, label_visibility="visible")
 if file_upload is not None:
     st.write(query(file_upload))
+if camera_input is not None:
+    st.write(query(camera_input))
 
