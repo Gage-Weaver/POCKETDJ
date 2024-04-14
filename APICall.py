d05559c6e1b9c0d00426850eca7c979c3324a8cf
Gage Weaver <Gage_Weaver@ku.edu>
2024-04-14 00:08:50 -0500

    Add more comments for APICall
    Add comments

--- APICall.py
+++ APICall.py
@@ -3,12 +3,12 @@ import requests
 
 pipe = pipeline("image-classification", model="trpakov/vit-face-expression")
 def localQuery(filename): #Define Function for Local Queries
-    return pipe(filename)
+    return pipe(filename) #return the value for local model query
 
 def onlineQuery(filename): #Define backup function for online queries
-    Api_Token = "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"
-    API_URL = "https://api-inference.huggingface.co/models/trpakov/vit-face-expression"
+    Api_Token = "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" #Define Api Token Key
+    API_URL = "https://api-inference.huggingface.co/models/trpakov/vit-face-expression" #Define url for Api Search
     headers = {"Authorization": f"Bearer {Api_Token}"}
     data = filename
-    response = requests.post(API_URL, headers=headers, data=data)
-    return response.json()
+    response = requests.post(API_URL, headers=headers, data=data) #Make an api call
+    return response.json()  #Return the result
